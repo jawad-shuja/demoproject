9b715801e367ecec1f4e2738b3a28431ea8f330b
Jawad Shuja <jawad.shuja@gmail.com>
2015-08-17 15:47:01 +0500

    Attachment: Added attachment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150817055723) do
+ActiveRecord::Schema.define(:version => 20150817104409) do
+
+  create_table "attachments", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
