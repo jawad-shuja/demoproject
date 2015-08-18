110fec6472123d95b288793056ed67b84012be92
Jawad Shuja <jawad.shuja@gmail.com>
2015-08-18 19:06:17 +0500

    Product: Added images to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   PER_PAGE = 5
-  attr_accessible :body, :price, :title
+  has_many :attachments, as: :attachable, dependent: :destroy
+  accepts_nested_attributes_for :attachments, allow_destroy: true
+  attr_accessible :body, :price, :title, :attachments_attributes
   scope :ordered, -> { order('created_at DESC') }
 end
