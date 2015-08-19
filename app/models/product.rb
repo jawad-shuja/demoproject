33a03a4824f732f84f92dec00c802e941c8d627f
Jawad Shuja <jawad.shuja@gmail.com>
2015-08-19 19:25:50 +0500

    Product: Added model validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,11 @@ class Product < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   belongs_to :user
   accepts_nested_attributes_for :attachments, allow_destroy: true
+
+  validates :title, presence: true, length: { minimum: 5, maximum: 70 }
+  validates :body, presence: true, length: { minimum: 25, maximum: 300 }
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0 }
+
   attr_accessible :body, :price, :title, :attachments_attributes
   scope :ordered, -> { order('created_at DESC') }
 end
