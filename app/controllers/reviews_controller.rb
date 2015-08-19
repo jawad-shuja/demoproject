ec6ea0977de0edac8e15e2c45303dc5bf08c2726
Jawad Shuja <jawad.shuja@gmail.com>
2015-08-19 15:39:21 +0500

    Review: Added non-ajax Review crud operations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,17 @@
 class ReviewsController < ApplicationController
   before_filter :set_review, only: [:show, :edit, :update, :destroy]
-  before_filter :set_product, only: [:create]
+  before_filter :set_product
   respond_to :html
 
   def index
     @reviews = Review.all
-    respond_with(@reviews)
   end
 
   def show
-    respond_with(@review)
   end
 
   def new
     @review = Review.new
-    respond_with(@review)
   end
 
   def edit
@@ -24,19 +21,19 @@ class ReviewsController < ApplicationController
     @review = @product.reviews.new(params[:review])
     @review.save
     respond_to do |format|
-      format.html { redirect_to @product }
+      format.html { redirect_to product_reviews_path(@product) }
       format.js
     end
   end
 
   def update
     @review.update_attributes(params[:review])
-    respond_with(@review)
+    respond_with(@product, @review)
   end
 
   def destroy
     @review.destroy
-    respond_with(@review)
+    respond_with(@product, @review)
   end
 
   private
