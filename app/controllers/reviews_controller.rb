d2386841eb148fbf926d67e8b4c54c564ec10ad3
Jawad Shuja <jawad.shuja@gmail.com>
2015-08-21 11:15:45 +0500

    Review: Allowed Product's owner to delete Product's Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,8 @@ class ReviewsController < ApplicationController
   before_filter :set_review, only: [:show, :edit, :update, :destroy]
   before_filter :set_product
   before_filter :restrict_owner, except: [:index, :show, :destroy]
-  before_filter :validate_user, only: [:edit, :update, :destroy]
+  before_filter :validate_user, only: [:edit, :update]
+  before_filter :validate_product_owner, only: [:destroy]
   respond_to :html
 
   def index
@@ -60,7 +61,13 @@ class ReviewsController < ApplicationController
 
     def validate_user
       unless owner?(@review.user_id)
-        redirect_to product_reviews_path(@product), notice: "You can only edit or delete your own reviews!"
+        redirect_to product_reviews_path(@product), notice: "You cannot edit this review!"
+      end
+    end
+
+    def validate_product_owner
+      unless owner?(@review.user_id) || owner?(@product.user_id)
+        redirect_to product_reviews_path(@product), notice: "You cannot delete this review!"
       end
     end
 end
