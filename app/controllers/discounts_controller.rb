c3d4e7d6ebb698d5485ab3abc03f07dafc25878d
Jawad Shuja <jawad.shuja@gmail.com>
2015-08-27 18:51:36 +0500

    Discount: Created constant for discount percentage

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,6 +1,7 @@
 class DiscountsController < ApplicationController
   def validate
     @discount = Discount.find_by_coupon(params[:coupon])
+    @discount_percentage = 1 - (Discount::DISCOUNT_PERCENTAGE.to_f / 100)
     respond_to do |format|
       format.js
     end
--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,4 @@
 class Discount < ActiveRecord::Base
+  DISCOUNT_PERCENTAGE = 10
   attr_accessible :coupon, :is_valid
 end
