16746e72b7839ede48d43113e471a7e58a9800c1
Jawad Shuja <jawad.shuja@gmail.com>
2015-09-03 18:13:36 +0500

    Fixed spacing and notation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,5 +36,5 @@ Demoproject::Application.configure do
   config.assets.debug = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 end
